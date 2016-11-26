d78ec181d96ff5188799e20ff9580978af11f765
Chris Hitchcott <hitchcott@gmail.com>
2016-11-25 17:42:06 -0800

    add `key` to inserted objects + code style refactor

--- src/reducer.js
+++ src/reducer.js
@@ -8,11 +8,24 @@ export default function (actionNames) {
   return (state = {}, action) => {
     switch (action.type) {
       case actionNames.ADD:
-        return mapKeys({ ...state, ...action.items.reduce((obj, acc) => ({ ...obj, [uuid()]: acc }), {}) });
+        return mapKeys({
+          ...state,
+          ...action.items.reduce((obj, acc) => {
+            const key = acc.key || uuid();
+            return { ...obj, [key]: { ...acc, key } };
+          }, {}),
+        });
       case actionNames.UPDATE:
-        return { ...state, ...Object.keys(action.items).reduce((obj, key) => ({ ...obj, [key]: { ...state[key], ...action.items[key] } }), {}) };
+        return {
+          ...state,
+          ...Object.keys(action.items).reduce((obj, key) => {
+            return { ...obj, [key]: { ...state[key], ...action.items[key] } };
+          }, {}),
+        };
       case actionNames.REMOVE:
-        return mapKeys(Object.keys(state).reduce((obj, key) => (action.ids.indexOf(key) !== -1 ? obj : { ...obj, [key]: state[key] }), {}));
+        return mapKeys(Object.keys(state).reduce((obj, key) => {
+          return action.ids.indexOf(key) !== -1 ? obj : { ...obj, [key]: state[key] };
+        }, {}));
       default:
         return state;
     }
